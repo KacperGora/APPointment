bc32f91b0be11bdecc57d6b634c3cf760ac368da
KacperGora <kacpergoora@gmail.com>
2022-11-22 12:51:35 +0100

    Added settings & incomings chart, added saloon options

---
+++ components/Settings/Customers/AddNewCustomer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Button, View } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+
+const AddNewCustomer = () => {
+  return (
+    <View>
+      <TextInput placeholder="Imię" />
+      <TextInput placeholder="Nazwisko" />
+      <TextInput placeholder="Numer telefonu" />
+      <TextInput placeholder="Uwagi" />
+      <Button title="Dodaj" />
+    </View>
+  );
+};
+
+export default AddNewCustomer;
